3d5a04927a1301e35dc087a0e0bab4dfc980e02d
chichunchen <chichunchen844@gmail.com>
2018-04-14 15:45:40 -0400

    Improve lockfree queue

--- a4/src/test_lockfree_queue.cpp
+++ a4/src/test_lockfree_queue.cpp
@@ -2,33 +2,34 @@
 #include <thread>
 #include "lockfree_queue.h"
 
+#define THREAD_NUM	16
+#define NODE_NUM	1000
+
 void test_enqueue(msqueue<int> *queue, int tid) {
-    int start = tid * 10;
-    int end = start + 10;
+    int start = tid * NODE_NUM;
+    int end = start + NODE_NUM;
     for (int i = start; i < end; i++) {
         queue->enqueue(i);
     }
 }
 
 void test_dequeue(msqueue<int> *queue) {
-    for (int i = 0; i < 10; i++) {
-        queue->dequeue();
-    }
+	int node;
+	while (queue->dequeue(node));
 }
 
 void test_msqueue() {
-    const int n = 8;
-    std::thread thread_arr[n];
+    std::thread thread_arr[THREAD_NUM];
 
     auto *queue = new msqueue<int>;
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < THREAD_NUM; i++) {
         thread_arr[i] = std::thread(test_enqueue, queue, i);
     }
     for (auto &i : thread_arr) {
         i.join();
     }
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < THREAD_NUM; i++) {
         thread_arr[i] = std::thread(test_dequeue, queue);
     }
     for (auto &i : thread_arr) {
